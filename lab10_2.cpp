c44e7bbcf18ef5742d61c6f9d5ea33804a715f4e
kanthidaja <kanthida_khun@cmu..ac.th>
2024-12-17 16:01:19 +0700

    completed

--- lab10_2.cpp
+++ lab10_2.cpp
@@ -7,11 +7,16 @@ int main (){
 	ifstream source;
 	ofstream dest;
 	source.open("cheerbook.txt") ;
+	string textline;
 	dest.open("cheerbook_copy.txt");
-	
+	getline(source, textline);
+	dest << "-------------------- BOOM ---------------------" << "\n";
+	while(getline(source,textline)){
+		dest << textline << "\n";
+	}
+	dest << "-------------------- HA!! ---------------------\n";
 	//กรอก Code เฉพาะส่วนนี้ ให้สอดคล้องกับโจทย์และ Code ส่วนอื่น ๆ 
 	//โดยห้ามแก้ไข Code ในส่วนอื่น ๆ
-
     source.close();
     dest.close();
 	return 0;
